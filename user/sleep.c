3b058aa2692d9b7093e7eb0398cbfa7ed98bd276
HuJianfeng <huzi.master@outlook.com>
2019-11-17 18:18:46 +0800

    add: sleep.

---
+++ user/sleep.c
@@ -0,0 +1,15 @@
+#include "kernel/types.h"
+#include "user/user.h"
+
+int
+main(int argc, char *argv[])
+{
+  char err_msg[] = "too few args!\n";
+  if (argc < 2) {
+    write(2, err_msg, strlen(err_msg));
+  } else {
+    int seconds = atoi(argv[1]);
+    sleep(seconds);
+  }
+  exit();
+}
