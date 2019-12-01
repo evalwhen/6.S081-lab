6125bedbb8774ddeb59e50195dfc79d9fb429739
HuJianfeng <huzi.master@outlook.com>
2019-12-01 22:16:06 +0800

    add: vmprint.

--- kernel/vm.c
+++ kernel/vm.c
@@ -444,3 +444,32 @@ copyinstr(pagetable_t pagetable, char *dst, uint64 srcva, uint64 max)
     return -1;
   }
 }
+
+void vmprint(pagetable_t pt) {
+  // there are 2^9 = 512 PTEs in a page table.
+  static int level = 0;
+
+  if (level == 3) {
+    return;
+  }
+
+  if (level == 0) {
+    printf("page table %p\n", pt);
+  }
+
+  for (int i = 0; i < 512; i++) {
+    pte_t pte = pt[i];
+    if (pte & PTE_V) {
+      // this PTE points to a lower-level page table.
+      uint64 child = PTE2PA(pte);
+      for (int j = level; j >= 0; j--) {
+        printf(".. ");
+      }
+      printf("%d ", i);
+      printf("pte %p pa %p\n", pte, child);
+      level++;
+      vmprint((pagetable_t)child);
+      level--;
+    }
+  }
+}
